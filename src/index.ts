f472eb6c5eb55dc4ff0f608d7a573902a2bacca7
kanisorn <kanisorn.peach@gmail.com>
2019-01-09 13:24:18 +0700

    add typedoc feature and ignore doc folder

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,15 @@ import Web3 from 'web3';
 import BN from 'bn.js';
 
 const web3: Web3 = new Web3(new Web3.providers.HttpProvider('https://rinkeby.infura.io/v3/7288751bb1014a7d8012057ca9303bed'));
-const getBalance = async (address: string): Promise<BN> => {
+
+/**
+ * This is a function with multiple arguments and a return value.
+ * @param address  This is a string parameter.
+ * 
+ * @returns This is balance of address.
+ *
+ */
+export const getBalance = async (address: string): Promise<BN> => {
     return (await web3.eth.getBalance(address));
 };
 
