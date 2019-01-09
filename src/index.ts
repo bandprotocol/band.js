e2a1cef1b8e0471aa6700ba126005bda930fb66e
kanisorn <kanisorn.peach@gmail.com>
2019-01-09 10:45:28 +0700

    use BN type

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import Web3 from 'web3';
+import BN from 'bn.js';
 
 const web3: Web3 = new Web3(new Web3.providers.HttpProvider('https://rinkeby.infura.io/v3/7288751bb1014a7d8012057ca9303bed'));
-const getBalance = async (address: string): Promise<string> => {
-    return (await web3.eth.getBalance(address)).toString();
+const getBalance = async (address: string): Promise<BN> => {
+    return (await web3.eth.getBalance(address));
 };
 
 ;(async () => {
-    console.log("Your balance:", await getBalance('0xCE3E5C43bcF9BB937D50653BB830723fa477ED1E'));
+    console.log("Your balance:", (await getBalance('0xCE3E5C43bcF9BB937D50653BB830723fa477ED1E')).toString());
 })()
