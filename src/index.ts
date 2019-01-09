da835765cb73cfe41dfd5702ec07b1b8c313bee0
kanisorn <kanisorn.peach@gmail.com>
2019-01-09 18:10:38 +0700

    implement BandprotocolClient

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,21 @@
 import Web3 from 'web3';
-import BN from 'bn.js';
+import BandProtocolClient from './service'
 
-const web3: Web3 = new Web3(new Web3.providers.HttpProvider('https://rinkeby.infura.io/v3/7288751bb1014a7d8012057ca9303bed'));
+const test = new Web3.providers.HttpProvider('https://rinkeby.infura.io/v3/7288751bb1014a7d8012057ca9303bed');
 
-/**
- * This is a function with multiple arguments and a return value.
- * @param address  This is a string parameter.
- * 
- * @returns This is balance of address.
- *
- */
-export const getBalance = async (address: string): Promise<BN> => {
-    return (await web3.eth.getBalance(address));
-};
+const bandProtocolClient = new BandProtocolClient();
+const bandClient = bandProtocolClient.make({
+    provider: test
+})
+
+console.log(bandClient)
 
 ;(async () => {
-    console.log("Your balance:", (await getBalance('0xCE3E5C43bcF9BB937D50653BB830723fa477ED1E')).toString());
+    const x = await bandClient.getNetwork();
+    console.log("Network: ", x)
+})()
+
+;(async () => {
+    const x = await bandClient.getBalance();
+    console.log("Owner Balance:", x)
 })()
