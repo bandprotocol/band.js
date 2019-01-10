4692ee0b17dc04f2d7f8376479d8c4a2a1e360c6
kanisorn <kanisorn.peach@gmail.com>
2019-01-10 16:02:13 +0700

    update getbalance function to calling for bandapi
    Summary: update getBalance function to calling address's balance from bandapi
    
    Test Plan: n/a
    
    Reviewers: swit
    
    Differential Revision: http://code.bandprotocol.com/D195

--- src/service/index.ts
+++ src/service/index.ts
@@ -1,43 +1,55 @@
 import Web3 from 'web3'
-import BN from 'bn.js';
-import { Provider } from 'web3/providers';
+// import BN from 'bn.js'
+import { Provider } from 'web3/providers'
+import axios from 'axios'
+import * as config from '../config.json'
 
-type BandProtocolClientConstructorArgs = {
-    provider?: Provider
+interface BandProtocolClientConstructorArgs {
+  provider?: Provider
+}
+
+interface GetBalance {
+  result: {
+    balance: string
+  }
 }
 
 export default class BandProtocolClient {
-    // field
-    provider?: Provider
+  // field
+  provider?: Provider
 
-    constructor(provider?: Provider) {
-       this.provider = provider
-    }
+  constructor(provider?: Provider) {
+    this.provider = provider
+  }
 
-    make(args: BandProtocolClientConstructorArgs) {
-        return new BandProtocolClient(args.provider)
-    }
+  make(args: BandProtocolClientConstructorArgs) {
+    return new BandProtocolClient(args.provider)
+  }
 
-    async getNetwork(): Promise<string>{
-        const web3 = new Web3(this.provider)
-        const networkId = await web3.eth.net.getId()
-        switch (networkId) {
-            case 1:
-                return 'Mainnet'
-            case 3:
-                return 'Ropsten'
-            case 4:
-                return 'Rinkeby'
-            case 42:
-                return 'Kovan' 
-            default:
-                return 'Unknown'
-        }
+  async getNetwork(): Promise<string> {
+    const web3 = new Web3(this.provider)
+    const networkId = await web3.eth.net.getId()
+    switch (networkId) {
+      case 1:
+        return 'Mainnet'
+      case 3:
+        return 'Ropsten'
+      case 4:
+        return 'Rinkeby'
+      case 42:
+        return 'Kovan'
+      default:
+        return 'Unknown'
     }
-    
-    async getBalance(): Promise<BN> {
-        const web3 = new Web3(this.provider)
-        return await web3.eth.getBalance('0xCE3E5C43bcF9BB937D50653BB830723fa477ED1E')
-    }
+  }
 
+  async getBalance(): Promise<string> {
+    return await axios
+      .get<GetBalance>(
+        config.api + '/balance/0x9fbE9e0869c32C4622fCa27879A6d8bC2fD79A5D',
+      )
+      .then(res => {
+        return res.data.result.balance
+      })
+  }
 }
